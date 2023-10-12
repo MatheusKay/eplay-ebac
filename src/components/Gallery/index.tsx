8f77cfe1a7036a60f52c6f7d8f9de42432e7e5cc
MatheusKay <mathueskayquebatistasilva712@gmail.com>
2023-10-11 23:01:46 -0300

    Finalizando a Galeria de fotos da seção de jogos

--- src/components/Gallery/index.tsx
+++ src/components/Gallery/index.tsx
@@ -1,26 +1,128 @@
+import { useState } from 'react'
+
 import Section from '../Section'
 
-import { Item, Items } from './style'
+import { Item, Items, Action, Modal } from './style'
 
 import diablo from '../../assets/imagens/diablo.png'
+import hogwarts from '../../assets/imagens/fundo_hogwarts.png'
 
-const Gallery = () => (
-  <Section title="Galeria" background="black">
-    <Items>
-      <Item>
-        <img src={diablo} alt="Capa do Diablo IV" />
-      </Item>
-      <Item>
-        <img src={diablo} alt="Capa do Diablo IV" />
-      </Item>
-      <Item>
-        <img src={diablo} alt="Capa do Diablo IV" />
-      </Item>
-      <Item>
-        <img src={diablo} alt="Capa do Diablo IV" />
-      </Item>
-    </Items>
-  </Section>
-)
+import iconZoom from '../../assets/imagens/zoom.png'
+import iconPlay from '../../assets/imagens/play.png'
+import iconFechar from '../../assets/imagens/fechar.png'
+
+import { Container } from '../../styles'
+
+interface GallaryItem {
+  type: 'image' | 'video'
+  url: string
+}
+
+const mock: GallaryItem[] = [
+  {
+    type: 'image',
+    url: diablo
+  },
+  {
+    type: 'image',
+    url: hogwarts
+  },
+  {
+    type: 'video',
+    url: 'https://www.youtube.com/embed/78CP8na1Fpo?si=i3ecxqgTA7PgZOt0'
+  }
+]
+
+type Props = {
+  defaultCover: string
+  name: string
+}
+
+interface ModalState extends GallaryItem {
+  isVisible: boolean
+}
+
+const Gallery = ({ defaultCover, name }: Props) => {
+  const [modal, setModal] = useState<ModalState>({
+    isVisible: false,
+    type: 'image',
+    url: ''
+  })
+
+  const getMediaCover = (item: GallaryItem) => {
+    if (item.type === 'image') return item.url
+    return defaultCover
+  }
+
+  const getMediaIcon = (item: GallaryItem) => {
+    if (item.type === 'image') return iconZoom
+    return iconPlay
+  }
+
+  const closeModal = () => {
+    setModal({
+      isVisible: false,
+      type: 'image',
+      url: ''
+    })
+  }
+
+  return (
+    <>
+      <Section title="Galeria" background="black">
+        <Items>
+          {mock.map((media, index) => (
+            <Item
+              key={media.url}
+              onClick={() => {
+                setModal({
+                  isVisible: true,
+                  type: media.type,
+                  url: media.url
+                })
+              }}
+            >
+              <img
+                src={getMediaCover(media)}
+                alt={`Midia ${index + 1} do ${name}`}
+              />
+              <Action>
+                <img
+                  src={getMediaIcon(media)}
+                  alt="Clique para visualizar a midia"
+                />
+              </Action>
+            </Item>
+          ))}
+        </Items>
+      </Section>
+      <Modal className={modal.isVisible ? 'visivel' : ''}>
+        <Container>
+          <header>
+            <h4>{name}</h4>
+            <img
+              src={iconFechar}
+              alt="Icone de fechar"
+              onClick={() => {
+                closeModal()
+              }}
+            />
+          </header>
+          {modal.type === 'image' ? (
+            <img src={modal.url} alt="" />
+          ) : (
+            <iframe src={modal.url} frameBorder={0} />
+          )}
+        </Container>
+        <div
+          onClick={() => {
+            closeModal()
+          }}
+          className="overlay"
+        ></div>
+      </Modal>
+    </>
+  )
+}
 
 export default Gallery
--- src/components/Gallery/style.ts
+++ src/components/Gallery/style.ts
@@ -1,18 +1,102 @@
 import styled from 'styled-components'
-import { cores } from '../../styles'
+
+import { Container, cores } from '../../styles'
 
 export const Items = styled.ul`
   display: flex;
 `
 
-export const Item = styled.li`
-  margin-right: 16px;
+export const Action = styled.div`
+  height: 100%;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  top: 0;
+  left: 0;
+  background-color: rgba(0, 0, 0, 0.73);
+  opacity: 0;
+  transition: opacity 0.5s ease;
 
   img {
+    cursor: pointer;
+  }
+`
+
+export const Item = styled.li`
+  margin-right: 16px;
+  position: relative;
+
+  > img {
     width: 150px;
     height: 150px;
     border: 2px solid ${cores.branco};
     border-radius: 8px;
     object-fit: cover;
+  }
+
+  &:hover {
+    ${Action} {
+      opacity: 1;
+      transition: opacity 0.5s ease;
+    }
+  }
+`
+
+export const Modal = styled.div`
+  width: 100%;
+  height: 100%;
+  display: none;
+  align-items: center;
+  justify-content: center;
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 1;
+
+  &.visivel {
+    display: flex;
+  }
+
+  .overlay {
+    height: 100%;
+    width: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    background-color: rgba(0, 0, 0, 0.73);
+  }
+
+  ${Container} {
+    max-width: 960px;
+    position: relative;
+    z-index: 1;
+
+    header {
+      display: flex;
+      justify-content: space-between;
+      margin-bottom: 24px;
+
+      h4 {
+        font-size: 18px;
+        font-weight: bold;
+      }
+
+      img {
+        cursor: pointer;
+      }
+    }
+
+    > img,
+    iframe {
+      width: 100%;
+      display: block;
+    }
+
+    iframe {
+      width: 100%;
+      height: 480px;
+    }
   }
 `
